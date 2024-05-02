653bce769f6876c337c0d7cefca9815e43ca2bdf
Sarajit-mondal <bdsarajit499@gmail.com>
2024-05-02 13:31:59 +0600

    Navbar complete

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,5 +1,21 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../pages/shard/Navbar";
+import Footer from "../pages/shard/Footer";
+
 function MainLayout() {
-  return <div>MainLayout</div>;
+  return (
+    <div>
+      <header>
+        <Navbar></Navbar>
+      </header>
+      <main>
+        <Outlet></Outlet>
+      </main>
+      <footer>
+        <Footer></Footer>
+      </footer>
+    </div>
+  );
 }
 
 export default MainLayout;

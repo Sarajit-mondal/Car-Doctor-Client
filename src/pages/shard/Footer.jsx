f95de57b1b1a85e400d04d759e5ec6c152702f8e
Sarajit-mondal <bdsarajit499@gmail.com>
2024-05-02 13:43:59 +0600

    working footer

--- src/pages/shard/Footer.jsx
+++ src/pages/shard/Footer.jsx
@@ -1,5 +1,43 @@
+import logo from "../../assets/logo.svg";
+import { FaFacebook, FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer p-10 bg-base-200 text-base-content">
+      <aside>
+        <img src={logo} alt="" className="size-16" />
+        <p className="max-w-52">
+          Edwin Diaz is a software and web technologies engineer, a life coach
+          trainer who is also a serial .
+        </p>
+        <div>
+          <span>
+            <FaFacebook></FaFacebook>
+          </span>
+        </div>
+      </aside>
+      <nav>
+        <h6 className="footer-title">Services</h6>
+        <a className="link link-hover">Branding</a>
+        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Company</h6>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Legal</h6>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </nav>
+    </footer>
+  );
 }
 
 export default Footer;
